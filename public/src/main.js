f72835ec2749eef91d6a407778733d192552ad7c
ChoiJIN <zuharu@gmail.com>
2016-01-13 19:47:30 +0900

    T2 re finish

--- public/src/main.js
+++ public/src/main.js
@@ -1,60 +1,108 @@
 var g_mainmenu = [
-    "res/HelloWorld.png"
+    "res/colorbg.jpg", "res/minion.png"
 ]
+var GameLayer = cc.Layer.extend({
+    ctor: function () {
+        this._super();
+        this.init();
+    },
+    init: function () {
+        this._super();
+        var size = cc.director.getWinSize();
+
+        var bgSprite = cc.Sprite.create("res/colorbg.jpg");
+        bgSprite.setPosition(size.width / 2, size.height / 2);
+        bgSprite.setScale(1.0);
+        this.addChild(bgSprite, 0);
+        
+        var minionSprite = cc.Sprite.create("res/minion.png");
+        cc.eventManager.addListener(minionListener, minionSprite);
+        this.addChild(minionSprite, 1);
+  
+        var label = cc.LabelTTF.create("Hello World", "Arial", 40);
+        label.setPosition(size.width / 2, size.height / 2);
+        this.addChild(label, 1);
+
+    },
+    onEnter: function () {
+        this._super();
+
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            swallowTouches: true,
+            onTouchBegan: this.onTouchBegan,
+            onTouchMoved: this.onTouchMoved,
+            onTouchEnded: this.onTouchEnded,
+        }, this);
+    },
+
+    onTouchBegan: function (touch, event) {
+        var tp = touch.getLocation();
+        console.log('onTouchBegan:' + tp.x.toFixed(2) + ',' + tp.y.toFixed(2));
+        return true;
+    },
+
+    onTouchMoved: function (touch, event) {
+        var tp = touch.getLocation();
+        console.log('onTouchMOved:' + tp.x.toFixed(2) + ',' + tp.y.toFixed(2));
+    },
+
+    onTouchEnded: function (touch, event) {
+        var tp = touch.getLocation();
+        console.log('onTouchEnded:' + tp.x.toFixed(2) + ',' + tp.y.toFixed(2));
+    }
+});
+
+GameLayer.scene = function () {
+    var scene = new cc.Scene;
+    var layer = new GameLayer();
+    scene.addChild(layer);
+
+    return scene;
+};
 
 window.onload = function () {
+
     cc.game.onStart = function () {
+        cc.view.adjustViewPort(false);
+
         cc.view.resizeWithBrowserSize(true);
-        var policy = new cc.ResolutionPolicy(cc.ContainerStrategy.EQUAL_TO_FRAME, cc.ContentStrategy.SHOW_ALL);
+        var policy = new cc.ResolutionPolicy(cc.ContainerStrategy.EQUAL_TO_FRAME, cc.ContentStrategy.SHOW_ALL   );
         cc.view.setDesignResolutionSize(1920, 1080, policy)
-        
         //load resources
         cc.LoaderScene.preload(g_mainmenu, function () {
-            var sprite = cc.Sprite.create("res/HelloWorld.png");
-            var minion = new MinionSprite();
-
-            var MyScene = cc.Scene.extend({
-                onEnter: function () {
-                    this._super();
-                    var size = cc.director.getWinSize();
-                    sprite.setPosition(size.width / 2, size.height / 2);
-                    sprite.setScale(2.0);
-                    this.addChild(sprite);
-
-                    var label = cc.LabelTTF.create("Hello World", "Arial", 40);
-                    label.setPosition(size.width / 2, size.height / 2);
-                    this.addChild(label);
-
-                    this.addChild(minion);
-                }
-            });
-            cc.director.runScene(new MyScene());
+            cc.director.runScene(GameLayer.scene());
         }, this);
-
     };
     cc.game.run("gameCanvas");
 };
 
-var MinionSprite = cc.Sprite.extend({
-    ctor: function () {
-        this._super();
-        this.initWithFile("res/minion.png");
-        cc.eventManager.addListener(listener.clone(), this);
-    }
-});
-
-var listener = cc.EventListener.create({
+var minionListener = cc.EventListener.create({
     event: cc.EventListener.TOUCH_ONE_BY_ONE,
     swallowTouches: true,
     onTouchBegan: function (touch, event) {
         var target = event.getCurrentTarget();
-        var location = target.convertToNodeSpace(touch.getLocation());
-        var targetSize = target.getContentSize();
-        var targetRectangle = cc.rect(0, 0, targetSize.width, targetSize.
-            height);
-        if (cc.rectContainsPoint(targetRectangle, location)) {
-            console.log("I picked a tile!!");
+
+        var locationInNode = target.convertToNodeSpace(touch.getLocation());
+        var s = target.getContentSize();
+        var rect = cc.rect(0, 0, s.width, s.height);
+
+        if (cc.rectContainsPoint(rect, locationInNode)) {
+            cc.log("sprite began... x = " + locationInNode.x + ", y = " + locationInNode.y);
+            target.opacity = 180;
+            return true;
         }
-        
+        return false;
+    },
+    onTouchMoved: function (touch, event) {
+        var target = event.getCurrentTarget();
+        var delta = touch.getDelta();
+        target.x += delta.x;
+        target.y += delta.y;
+    },
+    onTouchEnded: function (touch, event) {
+        var target = event.getCurrentTarget();
+        cc.log("sprite onTouchesEnded.. ");
+        target.setOpacity(255);
     }
 });
