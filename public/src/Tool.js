423bc8f8ec10870b6cbc2d0f123d1a07101ad5a9
HoraceVelmont <dragond7@naver.com>
2016-02-03 14:43:41 +0900

    T101 도구의 멋진 기능

--- public/src/Tool.js
+++ public/src/Tool.js
@@ -1,7 +1,25 @@
 var Tool = cc.Sprite.extend({
-    requiredElectricPower : null,
+    requiredElectricPower: null,
     ctor: function (name) {
-        this._super("res/"+name+".png");
-        
-    }    
+        this._super("res/" + name + ".png");
+        console.log(this);
+        var ref = this;
+        ref.visible = false;
+        cc.eventManager.addListener({
+            event: cc.EventListener.MOUSE,
+            onMouseDown: function (event) {
+                if (event.getButton() == cc.EventMouse.BUTTON_LEFT) {
+                    ref.x = event.x;
+                    ref.y = event.y;                     
+                    ref.visible = true;
+                }
+            },
+            onMouseUp : function (event) {
+                if(event.getButton() == cc.EventMouse.BUTTON_LEFT){
+                    ref.visible = false;
+                }
+            }
+        }, this);
+    }
+    
 }); 
